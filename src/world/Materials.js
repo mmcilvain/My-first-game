f190f09b8c21c1259f327e6dd604754a9459cf7a
mmcilvain <100654432+mmcilvain@users.noreply.github.com>
2026-08-01 23:28:36 -0400

    Add src/world/Materials.js

---
+++ src/world/Materials.js
@@ -0,0 +1,64 @@
+import * as THREE from 'three';
+import { createProceduralTextures } from './ProceduralTextures.js';
+
+export function createMaterialLibrary(renderer) {
+  const textures = createProceduralTextures(renderer);
+  const make = (textureName, options = {}) => new THREE.MeshStandardMaterial({
+    color: options.color ?? 0xffffff,
+    map: textures[textureName].map,
+    roughnessMap: textures[textureName].roughnessMap,
+    normalMap: textures[textureName].normalMap,
+    normalScale: new THREE.Vector2(options.normalScale ?? .18, options.normalScale ?? .18),
+    roughness: options.roughness ?? .7,
+    metalness: options.metalness ?? 0,
+    transparent: options.transparent ?? false,
+    opacity: options.opacity ?? 1,
+    side: options.side ?? THREE.FrontSide,
+  });
+
+  const glass = new THREE.MeshPhysicalMaterial({
+    color: 0x87d6cc, map: textures.glass.map, normalMap: textures.glass.normalMap,
+    normalScale: new THREE.Vector2(.035, .035), roughness: .14, metalness: .08,
+    transmission: .18, thickness: .08, transparent: true, opacity: .42,
+    clearcoat: .65, clearcoatRoughness: .12, side: THREE.DoubleSide,
+  });
+  const wet = new THREE.MeshPhysicalMaterial({
+    color: 0x385b58, map: textures.wet.map, roughnessMap: textures.wet.roughnessMap, normalMap: textures.wet.normalMap,
+    normalScale: new THREE.Vector2(.24, .24), roughness: .16, metalness: .28,
+    clearcoat: .72, clearcoatRoughness: .11,
+  });
+  const materials = {
+    concrete: make('concrete', { roughness: .92, normalScale: .28 }),
+    concreteDark: make('concrete', { color: 0x697572, roughness: .95, normalScale: .24 }),
+    metal: make('metal', { metalness: .72, roughness: .37, normalScale: .08 }),
+    metalDark: make('metal', { color: 0x5b7068, metalness: .82, roughness: .32, normalScale: .08 }),
+    tile: make('tile', { roughness: .78, normalScale: .18 }),
+    militaryPaint: make('paint', { color: 0x718d78, roughness: .68, normalScale: .16 }),
+    militaryDark: make('paint', { color: 0x263c35, roughness: .74, normalScale: .15 }),
+    glass,
+    glassFrame: make('metal', { color: 0x172826, metalness: .6, roughness: .39, normalScale: .08 }),
+    wet,
+    wood: make('wood', { roughness: .81, normalScale: .22 }),
+    fabric: make('fabric', { roughness: .96, normalScale: .22 }),
+    rubber: make('rubber', { roughness: .93, normalScale: .15 }),
+    warning: make('paint', { color: 0xd17d32, roughness: .5, metalness: .16, normalScale: .12 }),
+    hazard: make('paint', { color: 0xa53b2e, roughness: .62, metalness: .08, normalScale: .14 }),
+    rust: make('metal', { color: 0x774132, roughness: .64, metalness: .52, normalScale: .12 }),
+    trim: make('metal', { color: 0x8ca69c, roughness: .3, metalness: .78, normalScale: .06 }),
+    edge: new THREE.LineBasicMaterial({ color: 0x71958a, transparent: true, opacity: .34 }),
+    panel: make('paint', { color: 0x142823, roughness: .54, metalness: .46, normalScale: .1 }),
+    screen: new THREE.MeshStandardMaterial({ color: 0x9effe3, emissive: 0x28b895, emissiveIntensity: 2.4, roughness: .24, metalness: .08 }),
+    cyan: new THREE.MeshStandardMaterial({ color: 0x76e8c6, emissive: 0x164d3f, emissiveIntensity: 1.8, roughness: .3, metalness: .15 }),
+    amber: new THREE.MeshStandardMaterial({ color: 0xffb24d, emissive: 0x80300b, emissiveIntensity: 1.5, roughness: .3 }),
+    cyanHot: new THREE.MeshStandardMaterial({ color: 0xb7fff0, emissive: 0x2bdbc0, emissiveIntensity: 3.1, roughness: .2, metalness: .08 }),
+    amberHot: new THREE.MeshStandardMaterial({ color: 0xffd28a, emissive: 0xff6a15, emissiveIntensity: 2.7, roughness: .24, metalness: .06 }),
+    puddle: new THREE.MeshPhysicalMaterial({ color: 0x152c2e, roughness: .09, metalness: .34, clearcoat: 1, clearcoatRoughness: .08, transparent: true, opacity: .82, depthWrite: false }),
+    black: new THREE.MeshStandardMaterial({ color: 0x0b1110, roughness: .65, metalness: .35 }),
+    target: make('paint', { color: 0xb99776, roughness: .82, normalScale: .2 }),
+    targetAccent: make('paint', { color: 0xb64436, roughness: .62, metalness: .05, normalScale: .12 }),
+    decal: new THREE.MeshBasicMaterial({ color: 0x111c19, transparent: true, opacity: .72, depthWrite: false, polygonOffset: true, polygonOffsetFactor: -3, polygonOffsetUnits: -3 }),
+  };
+  materials._textures = textures;
+  materials.dispose = () => Object.values(materials).forEach((material) => material?.dispose?.());
+  return materials;
+}
